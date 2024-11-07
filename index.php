0f5a3706f74c1a6435be472c708b8a91b9561aa3
Your Name <you@example.com>
2024-11-07 11:07:42 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+$variable = "Eren CennetKuşu";
+// $args1 = 10;
+// $args2 = 20;
+// // print $args1 + $args2;
+// if($args1 == $args2){
+//     print "esittir";
+// }else{
+//     print "eşit değildir";
+// }
+// -------------
+
+$args1 = 20;
+$args2 = '20';
+if($args1 === $args2){
+    print "esittir";
+}else{
+    print "eşit değildir";
+}
+?>
