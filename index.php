af1a28e2113c5b081e017aee6ab8e4aa5582c6fc
Your Name <you@example.com>
2024-11-07 17:08:04 +0300

    diziler

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$variable = "Eren CennetKuşu";
+// $variable = "Eren CennetKuşu";
 // $args1 = 10;
 // $args2 = 20;
 // // print $args1 + $args2;
@@ -18,13 +18,52 @@ $variable = "Eren CennetKuşu";
 //     print "eşit değildir";
 // }
 //-------------------
-$args1 = 30;
-$args2 = 20;
-if($args1 === $args2){
-    print "esittir";
-}else if($args1 > $args2){
-    print $args1 .' : Büyüktür.';
-}else if($args1 < $args2){
-    print $args1 .' : küçüktür'. $args2;
-}
+// $args1 = 30;
+// $args2 = 20;
+// if($args1 === $args2){
+//     print "esittir";
+// }else if($args1 > $args2){
+//     print $args1 .' : Büyüktür.';
+// }else if($args1 < $args2){
+//     print $args1 .' : küçüktür'. $args2;
+// }
+
+// $veriable = "Kasım Yüksel & Eren CennetKuşu";
+// $args1 = 1;
+// $args2 = 20;
+// // print $args1 += $args2;
+// // print $args1 .= 50 ."<br>";
+// print $args1 -= 50;
+// -----------diziler-------------------
+// $array = array(
+//     "foo" => "bar",
+//     "bar" => "foo",
+// );
+//----dizilerde bu iki yöntemde aynı, istediğimizi kullanabiliriz-----
+// $array = array(
+//     "foo" => "bar",
+//     "bar" => "foo",
+// );
+
+// $array = [
+//     "foo" => "bar",
+//     "bar" => 10.1,
+// ];
+// // print_r($array);
+// var_dump($array);
+//--------------------
+// $array = 
+// [
+//     "product" => "bilgisayar",
+//     "color" => "siyah"
+// ];
+// print_r($array);
+//-----------------------
+$array = 
+[
+  'products'=> ["product" => "bilgisayar","color" => "siyah", "işlemci tipi" => ["i7", "RYZEN 7"]],
+  'books' =>  ["product" => "Kitap","color" => "siyah"],
+];
+// print_r($array);
+print_r($array["products"]["işlemci tipi"][0]);
 ?>
