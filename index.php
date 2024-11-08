36c32e2ab29da591de5a3be525f2f80e600315be
Your Name <you@example.com>
2024-11-09 01:23:59 +0300

    diziler_döngüler

--- index.php
+++ index.php
@@ -58,12 +58,111 @@
 //     "color" => "siyah"
 // ];
 // print_r($array);
+
 //-----------------------
-$array = 
-[
-  'products'=> ["product" => "bilgisayar","color" => "siyah", "işlemci tipi" => ["i7", "RYZEN 7"]],
-  'books' =>  ["product" => "Kitap","color" => "siyah"],
-];
+// $array = 
+// [
+//   'products'=> ["product" => "bilgisayar","color" => "siyah", "işlemci tipi" => ["i7", "RYZEN 7"]],
+//   'books' =>  ["product" => "Kitap","color" => "siyah"],
+// ];
+// // print_r($array);
+// print_r($array["products"]["işlemci tipi"][0]);
+//-----------------------------
+// $a = 10;
+// $b = 20;
+// print ($a > $b) ? "Büyüktür." : "Değildir.";
+
+// $a = null;
+// $b = 20;
+// //null kontrol
+// print $a ?? "Değersiz";
+// $a = "";
+// $b = 20;
+// //dolu boş kontrol
+// print $a ?? "Değersiz";
+
+
+//--------------------DÖNGÜLER ( foreach, for, while)-----------------
+// $array = 
+// [
+//     "product" => "bilgisayar",
+//     "color" => "siyah"
+// ];
+// foreach ($array as $key => $value) {
+//     if ($key !== 'product') {
+//         print 'Anahtar: ' .$key .', Değer: ' .$value."<hr/>";
+//     }
+// }
+
+// --------for-------
+// for ($i=0; $i < 15; $i++) { 
+//     print $i ."<hr/>";
+// }
+
+// $array = 
+// [
+//      "bilgisayar",
+//      "siyah",
+// ];
+// print count($array);
+// exit;
+// for ($i=0; $i < count($array); $i++) { 
+//     print $array[$i] . "<hr/>";
+// }
+
+//---------While----------
+// $array = 
+//     [
+//         "bilgisayar",
+//         "siyah",
+//         "kitap",
+//         "defter",
+//         "Kalem",
+//         "Çanta",
+//     ];
+// $a = 0;
+// while ($a <= 5){
+//     print $array[$a] ."<hr/>";
+//     $a++;
+// }
+
+// --------------------------
+// $array = 
+//     [
+//         "bilgisayar",
+//         "siyah",
+//         "kitap",
+//         "defter",
+//         "Kalem",
+//         "Çanta",
+//     ];
+//     // $new_array = array_map(function ($item){
+//     //     return $item = "";
+
+//     // }, $array);
+//     // var_dump($new_array);
+
+//     $new_array = array_map(function ($item){
+//         return "Kırtasiye : " .$item;
+
+//     }, $array);
+//     var_dump($new_array);
+
+// $string = "Bilgisayar,Kitap,Defter,Kalem,Silgi,Çanta";
+// $array = explode(",",$string);
 // print_r($array);
-print_r($array["products"]["işlemci tipi"][0]);
+
+//-------------------//
+$array = 
+    [
+        "bilgisayar",
+        "siyah",
+        "kitap",
+        "defter",
+        "Kalem",
+        "Çanta",
+    ];
+    $string = implode(',',$array);
+print_r($string); 
+
 ?>
