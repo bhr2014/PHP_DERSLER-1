2f05f78cf995c77fccab6dad41444cc671dd847c
Your Name <you@example.com>
2024-11-16 01:15:19 +0300

    değişkeneler-koşullu ifadeler-döngüler

---
+++ degiskenler.php
@@ -0,0 +1,37 @@
+<?php
+//------ÖN TANIMLI DEĞİŞKENLER-----
+
+$GLOBALS;
+$_SERVER;
+$_REQUEST;
+$_FILES;
+$_ENV;
+$_SESSION;
+$_COOKIE;
+$_GET;
+$_POST;
+$http_response_header;
+$argc;
+$argv1.$GLOBALS;
+$_SERVER;
+$_REQUEST;
+$_FILES;
+$_ENV;
+$_SESSION;
+$_COOKIE;
+$_GET;
+$_POST;
+$http_response_header;
+$argc;
+$argv;
+// değişkenler
+$string = " Ben bir değişkenim";
+$_veriables = "alt çizgi ile başlayabilirim";
+// $12veriables = "sayıyla başlayamam";
+
+$data = "Ben data değişkeniyim";
+
+$DATA =" Ben DATA değişkeniyim";
+
+print $data . $DATA;
+?>
